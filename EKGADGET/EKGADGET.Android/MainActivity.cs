72c23195f810937a432ee1ce4fbae0cc0b307ac0
keops3312 <keops_3312@hotmail.com>
2019-06-27 18:54:07 -0500

    UPDATE

--- EKGADGET/EKGADGET.Android/MainActivity.cs
+++ EKGADGET/EKGADGET.Android/MainActivity.cs
@@ -10,7 +10,11 @@ using ImageCircle.Forms.Plugin.Droid;
 
 namespace EKGADGET.Droid
 {
-    [Activity(Label = "EKGADGET", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "EKGADGET", 
+        Icon = "@mipmap/iconekgadget", 
+        Theme = "@style/MainTheme",
+        MainLauncher = false, 
+        ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
